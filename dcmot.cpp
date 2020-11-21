364e144185fab49cbbe588f49c7c04155266ff51
Julian Friedrich <julian.friedrich@gmx.at>
2020-11-21 20:35:17 +0100

    initial commit

---
+++ dcmot.cpp
@@ -0,0 +1,110 @@
+/*
+ * dcmot, a class used to operate a DC motor connected to a TB6612FNG driver by 
+ * using STM32Duino hardware timer support.
+ * 
+ * Copyright (C) 2020 Julian Friedrich
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * You can file issues at https://github.com/fjulian79/libdcmot/issues
+ */
+
+#include <dcmot.h>
+
+#include <string.h>
+
+DcMot::DcMot() : 
+      pTimer(0)
+    , A(0)
+    , B(0)
+    , Channel(0)
+    , ARR(0)
+    , Resolution(0)
+{
+    /* Nothing to do */
+}
+
+DcMot::DcMot(DcMotParams_t *pParms, uint32_t res)
+{
+    init(pParms, res);
+}
+
+void DcMot::init(DcMotParams_t *pParms, uint32_t res)
+{
+    pTimer = pParms->pTim;
+    A = pParms->A;
+    B = pParms->B;
+    Resolution = res;
+    ARR = pTimer->getOverflow() + 1;
+    Channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(pParms->Pwm),
+            PinMap_PWM));
+    pTimer->setMode(Channel, TIMER_OUTPUT_COMPARE_PWM1,
+            digitalPinToPinName(pParms->Pwm));
+    pTimer->setCaptureCompare(Channel, 0);
+    pTimer->resumeChannel(Channel);
+    
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+}
+
+void DcMot::set(int32_t val)
+{
+    uint32_t ccval = 0;
+
+    if(pTimer == 0)
+    {
+        return;
+    }
+
+    val = constrain(val, -Resolution, Resolution);
+
+    if (val == 0)
+    {
+        digitalWrite(A, LOW);
+        digitalWrite(B, LOW);
+    }
+    else if(val < 0)
+    {
+        val = -val;
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+    } 
+    else
+    {
+        digitalWrite(A, LOW);
+        digitalWrite(B, HIGH);
+    }
+
+    ccval = (ARR * val) / Resolution;
+    pTimer->setCaptureCompare(Channel, ccval);
+}
+
+void DcMot::stop(void)
+{
+    set(0);
+}
+
+void DcMot::ebreak(uint32_t val)
+{
+    uint32_t ccval = (ARR * val) / Resolution;
+    
+    if(pTimer == 0)
+    {
+        return;
+    }
+
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    pTimer->setCaptureCompare(Channel, ccval);
+}
---
+++ dcmot/dcmot.hpp
@@ -0,0 +1,153 @@
+/*
+ * dcmot, a class used to operate a DC motor connected to a TB6612FNG driver.
+ * 
+ * Copyright (C) 2020 Julian Friedrich
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * You can file issues at https://github.com/fjulian79/libdcmot/issues
+ */
+
+#ifndef DCMOT_HPP_
+#define DCMOT_HPP_
+
+#include <Arduino.h>
+#include <stdint.h>
+
+/**
+ * @brief Definition of the DcMot parameter structure.
+ * 
+ */
+typedef struct 
+{
+    /**
+     * @brief Pointer to the timer instance to use.
+     * 
+     * The timer has to be be initalized sperately, see README.md
+     */
+    HardwareTimer *pTim;
+
+    /**
+     * @brief The pin number connected to the A driver pin.
+     */
+    uint32_t A;
+    
+    /**
+     * @brief The pin number connected to the B driver pin.
+     */
+    uint32_t B;
+    
+    /**
+     * @brief The pin number connected to the PWM driver pin.
+     */
+    uint32_t Pwm;
+
+}DcMotParams_t;
+
+class DcMot
+{
+    public:
+
+        /**
+         * @brief Construct a new DcMot object
+         * 
+         * Attention, the object has to be initilaized before it can be used, 
+         * see init(...);
+         */
+        DcMot();
+
+        /**
+         * @brief Construct a new DcMot object
+         * 
+         * @param pParams   Pointer to the parameters.
+         * @param res       Optional user resuloution, defaults to 100.
+         *                  See README.md for details.
+         */
+        DcMot(DcMotParams_t *pParms, uint32_t res = 100);
+
+        /**
+         * @brief Innitializes DcMot object
+         * 
+         * @param pParams   Pointer to the parameters.
+         * @param res       Optional user resuloution, defaults to 100.
+         *                  See README.md for details.
+         */
+        void init(DcMotParams_t *pParms, uint32_t res = 100);
+
+        /**
+         * @brief Sets the motor speed including the direction.
+         * 
+         * The given value will be limmited by the configured resolution.
+         * 
+         * @param val   The new speed value.
+         *              If the value is zero the motor is idle.
+         *              If the value is positive the motor spins forward.
+         *              If the vlaue is negative the motor spins backward
+         */
+        void set(int32_t val);
+
+        /**
+         * @brief Stops the motor.
+         * 
+         * Equal to set(0)
+         */
+        void stop(void);
+
+        /**
+         * @brief Enables the eddy break.
+         * 
+         * The given value will be limmited by the configured resolution.
+         * 
+         * @param val   The amount of break to apply. The value is limmited
+         *              by the configured resolution.
+         */
+        void ebreak(uint32_t val);
+
+    private:
+
+        /**
+         * @brief Pointer to the used timer instance.
+         */
+        HardwareTimer *pTimer;
+        
+        /**
+         * @brief The pin number connected to the A driver pin. 
+         */
+        int A;
+
+        /**
+         * @brief The pin number connected to the B driver pin.
+         */
+        int B;
+
+        /**
+         * @brief The CC chanel used to generate the pwm signal.
+         */
+        uint8_t Channel;
+
+        /**
+         * @brief The The timer overflow value.
+         * 
+         * This is also the maximum value to be set at the cc channel.
+         */
+        uint32_t ARR;    
+
+        /**
+         * @brief The user space resultion to map to the cc channel.
+         * 
+         */
+        uint32_t Resolution;
+};
+
+#endif /* DCMOT_HPP_ */
